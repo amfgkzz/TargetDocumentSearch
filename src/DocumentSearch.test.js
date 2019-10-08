d1aa4537061bf4bc657ae97194471be0f550bdf3
Koua Xiong <kxiong517@gmail.com>
2019-10-08 08:44:59 -0500

    added document file with two million words

--- src/DocumentSearch.test.js
+++ src/DocumentSearch.test.js
@@ -1,9 +1,10 @@
+const fs = require("fs");
 const DocumentSearch = require("./DocumentSearch");
 
 test('testStringMatchSearch', function() {
     const searchEngine = new DocumentSearch();
-    const twoMillionSearchesArray = new Array(2000000).fill('the');
-    for ( let i = 0; i < twoMillionSearchesArray.length; i++ ) {
-        const query = twoMillionSearchesArray[i];
-    }
+    searchEngine.method = '1';
+    searchEngine.path = `${__dirname}/documents`;
+    let testArray = fs.readFileSync('/Users/kouaxiong/TargetChallenge/TargetSeachEngine/src/test_documents/two_million_words.txt', 'utf8').split(' ');
+    console.log(testArray);
 });
