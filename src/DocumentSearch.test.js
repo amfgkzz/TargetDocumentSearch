49c3fa93c06dc1eaf839accbf857cc71d172929f
Koua Xiong <kxiong517@gmail.com>
2019-10-08 08:25:13 -0500

    revising the test

--- src/DocumentSearch.test.js
+++ src/DocumentSearch.test.js
@@ -2,9 +2,8 @@ const DocumentSearch = require("./DocumentSearch");
 
 test('testStringMatchSearch', function() {
     const searchEngine = new DocumentSearch();
-    const twoMillionSearchesArray = new Array(2000000);
+    const twoMillionSearchesArray = new Array(2000000).fill('the');
     for ( let i = 0; i < twoMillionSearchesArray.length; i++ ) {
         const query = twoMillionSearchesArray[i];
-        console.log(query);
     }
 });
