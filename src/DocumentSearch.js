dea90a26d32756cd863e2b714b66cc6ff70bbf2a
Koua Xiong <kxiong517@gmail.com>
2019-10-08 03:43:24 -0500

    first method

--- src/DocumentSearch.js
+++ src/DocumentSearch.js
@@ -8,52 +8,75 @@ const rl = readline.createInterface({
 
 const DocumentSearch = module.exports = function () {
     return {
+        method: null,
+        path: `${__dirname}/documents`,
         query: null,
         results: null,
-        processDocuments: function (path = '/Users/kouaxiong/TargetChallenge/TargetSeachEngine/documents') {
-            fs.readdir(path, (error, files) => {
+
+        processDocuments: function () {
+            switch (this.method) {
+                case '1':
+                    this.stringMatchSearch();
+                    rl.close();
+                    break;
+                case '2':
+                    this.regularExpressionSearch();
+                    rl.close();
+                    break;
+                case '3':
+                    this.indexedSearch();
+                    rl.close();
+                    break;            
+                default:
+                    console.log('Invalid method.');
+                    rl.close();
+                    break;
+            }
+        },
+
+        stringMatchSearch: function () {
+            console.log("string match");
+            fs.readdir(path = this.path, (error, files) => {
                 if (error) {
                     console.log(error);
                     return;
                 }
                 files.forEach((file) => {
-                    console.log(file);
+                    // console.log(file);
                     fs.readFile(`${path}/${file}`, 'utf8', (error, data) => {
+                        console.log(path);
                         if (error) {
                             console.log(error);
                             return;
                         }
-                        console.log(data);
-                        rl.close();
+                        let words = data.split(' ');
+                        console.log(words);
                     });
+                    rl.close();
                 });
             });
+        },
+
+        regularExpressionSearch: function () {
+            console.log("reg exp");
+        },
+
+        indexedSearch: function () {
+            console.log("index search");
         }
     }
 }
 
-rl.question('Enter the search term: ', (answer) => {
-    const search = new DocumentSearch();
-    if (!!answer) {
-        rl.question('Search Method: 1) String Match  2) Regular Expression  3) Indexed - ', (newAnswer) => {
-            switch (newAnswer) {
-                case '1':
-                    console.log("STRING MATCH");
-                    search.processDocuments();
-                    break;
-                case '2':
-                    console.log("REG EXP");
-                    break;
-                case '3':
-                    console.log("INDEXED");
-                    break;
-                default:
-                    console.log("INVALID");
-                    break;
-            }
+rl.question('Enter the search term: ', (query) => {
+    const request = new DocumentSearch();
+    if (!!query) {
+        rl.question('Search Method: 1) String Match  2) Regular Expression  3) Indexed - ', (method) => {
+            request.method = method;
+            request.query = query;
+            request.processDocuments();
         });
     } else {
-        console.log(`Nope`);
+        console.log('Invalid search query.');
         rl.close();
     }
 });
